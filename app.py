a4d8bf8a696c6115ca445bc7683866236114b816
hackiku <ivan@pipewriter.io>
2023-11-09 22:17:53 +0100

    display matlab and python code samples below the button

--- app.py
+++ app.py
@@ -4,29 +4,37 @@ from openai import OpenAI
 # Load OpenAI API key
 openai_client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-gpt_prompt = """you will act as a matlab to python refactoring machine. I will paste matlab code and you will just output the refactored code in python, including not too verbose but useful comments. Important: do not output anything beside the code, not even the ```python code block formatting. Your response is powering an aerospace refactoring app so I need only the raw output. REQUIREMENTS: python 3.9, numpy, matplotlib, scipy, simpy, pandas. The code to refactor is: 
 # Define gpt-4 outside of Streamlit instance
+gpt_prompt = """you will act as a matlab to python and vice-versa refactoring machine. I will show you either matlab or python code and you will just output the refactored code in the other language, including not too verbose but useful comments. Important: do not output anything beside the refactored code, not even the ```python or ```matlab code block formatting. Your response is powering an aerospace refactoring app so I need only the raw output. Requirements.txt: python 3.9, numpy, matplotlib, scipy, simpy, pandas. The code to refactor is: 
 """
 
 # UI elements
-st.title('MATLAB to Python Refactorer')
-st.write('Paste MATLAB code and get Python equivalent refactored by GPT-4.')
+st.title('matlab 🔄 python refactor')
+st.write('Paste MATLAB or Python code and get Python equivalent refactored by GPT-4.')
 
 # Sample MATLAB code for demonstration purposes
-sample_matlab_code = """% Sample MATLAB code for CFD
-rho = 1.225; % density of air (kg/m^3)
-mu = 1.7894e-5; % dynamic viscosity of air (Pa.s)
-U_infinity = 40; % free stream velocity (m/s)
-Re_L = rho * U_infinity * L / mu; % Reynolds number based on length L"""
+sample_matlab_code = """% MATLAB code for plotting a function and its gradient
+[x, y] = meshgrid(-2:0.1:2, -2:0.1:2); % Create a 2D grid of x and y values
+f = x.^2 - y.^2;                       % Define the function f(x, y)
 
-# Define columns for the layout
-cols = st.columns(2)
+% Calculate the gradient of f
+[fx, fy] = gradient(f, 0.1, 0.1);
 
-with cols[0]:
-    matlab_code = st.text_area('MATLAB Code', value=sample_matlab_code, height=300, help="Paste your MATLAB code here.")
+% Plot the function
+surf(x, y, f);
+title('Function f(x, y) = x^2 - y^2 and its Gradient');
+hold on;
 
-with cols[1]:
-    python_code_placeholder = st.empty()
+% Plot the gradient vectors
+quiver(x, y, fx, fy, 'r');
+hold off;"""
+
+
+matlab_code = st.text_area('MATLAB Code', value=sample_matlab_code, height=300, help="Paste your MATLAB code here.")
+
+# Placeholders for MATLAB and Python code blocks
+matlab_code_block_placeholder = st.empty()
+python_code_block_placeholder = st.empty()
 
 # The actual translation logic
 if st.button('Translate MATLAB to Python'):
@@ -45,8 +53,13 @@ if st.button('Translate MATLAB to Python'):
         # Extract the code from the response
         translated_code = chat_completion.choices[0].message.content
 
-        # Update the Python code in the placeholder
-        python_code_placeholder.text_area("Python Code", value=translated_code, height=300, help="The translated Python code will appear here.")
+        # Display MATLAB and Python code side by side
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.code(matlab_code, language='matlab')
+        with col2:
+            st.code(translated_code, language='python')
 
     except Exception as e:
         st.error(f"Error in translation: {e}")
@@ -56,4 +69,4 @@ with st.sidebar:
     st.header('About')
     st.info('This app uses GPT-4 to refactor MATLAB code to Python. It is designed for educational purposes in the field of aerospace engineering.')
     # The slider is a placeholder and does not affect the temperature setting in the API call above
-    st.sidebar.slider("Temperature (placeholder)")
+    st.sidebar.slider("Response length")
