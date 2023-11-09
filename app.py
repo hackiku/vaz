695cd749a6968dc92416815c00281b576523bdcd
hackiku <ivan@pipewriter.io>
2023-11-09 23:37:55 +0100

    replace text area with placeholder that can be used as proof of concept

--- app.py
+++ app.py
@@ -22,16 +22,16 @@ def call_openai_api(prompt):
         return None
 
 # Define prompts
-refactor_prompt = """you will act as a matlab to python and vice-versa refactoring machine. I will show you either matlab or python code and you will just output the refactored code in the other language, including not too verbose but useful comments. Important: do not output anything beside the refactored code. Your response is powering an aerospace refactoring app so I need only the raw output. Requirements.txt: python 3.9, numpy, matplotlib, scipy, simpy, pandas. The code to refactor is: 
+refactor_prompt = """you will act as a matlab to python and vice-versa refactoring machine. I will show you either matlab or python code and you will just output the refactored code in the other language, including not too verbose but useful comments. Important: do not output anything beside the refactored code, as your response is powering an aerospace refactoring app so I need only the raw output. Of course keep in mind best practices and tools for each language and make the refactor functional (e.g. when doing python -> matlab, no need to import package, etc). The code to refactor is: 
 """
+# Requirements.txt: python 3.9, numpy, matplotlib, scipy, simpy, pandas
 
 # UI elements
 st.title('MATLAB 🔄 Python Refactor')
 st.write('Paste MATLAB or Python code and get its equivalent in the other language refactored by GPT-4.')
 
-# Sample MATLAB code for demonstration purposes
-sample_code = """% MATLAB code for plotting a function and its gradient
-[x, y] = meshgrid(-2:0.1:2, -2:0.1:2); % Create a 2D grid of x and y values
+# Sample MATLAB code used as placeholder
+placeholder_code = """[x, y] = meshgrid(-2:0.1:2, -2:0.1:2); % Create a 2D grid of x and y values
 f = x.^2 - y.^2;                       % Define the function f(x, y)
 
 % Calculate the gradient of f
@@ -46,12 +46,14 @@ hold on;
 quiver(x, y, fx, fy, 'r');
 hold off;"""
 
-user_code = st.text_area('Code', value=sample_code, height=300, help="Paste your MATLAB or Python code here.")
-
+# user inputs
+user_code = st.text_area('Code to refactor', placeholder=placeholder_code, height=300, help="Paste your MATLAB or Python code here.")
 word_count = st.sidebar.slider("Word Count", min_value=50, max_value=300, value=150, step=1)
 
-# The actual translation logic
+# refactor logic
 if st.button('Translate Code'):
+    if not user_code: # if no user input, refactor placeholder code as demonstration
+        user_code = placeholder_code
     refactored_code = call_openai_api(refactor_prompt + user_code)
     if refactored_code:
         col1, col2 = st.columns(2)
@@ -60,9 +62,9 @@ if st.button('Translate Code'):
         with col2:
             st.code(refactored_code, language='python')
 
-# The actual interpretation logic
+# interpretation logic
 if st.button('Interpret Code'):
-    interpret_prompt = f"""you will interpret the following MATLAB or python code from an aerospace engineering perspective (in particular, CFD), explaining its purpose and functionality in simple terms. Make your response {word_count} words long. The code to interpret is:
+    interpret_prompt = f"""you will interpret the following MATLAB or python code from an aerospace engineering perspective, explaining its purpose and functionality in simple terms. Reprint code samples and formulas in markdown-compatible format whenever possible. Make your response {word_count} words long. The code to interpret is:
     """
     interpretation = call_openai_api(interpret_prompt.replace("$word_count", str(word_count)) + user_code)
     if interpretation:
@@ -73,6 +75,3 @@ if st.button('Interpret Code'):
 with st.sidebar:
     st.header('About')
     st.info('This app uses GPT-4 to refactor and interpret code. It is designed for educational purposes in the field of aerospace engineering.')
-    # Add a slider for max_tokens in the sidebar or main app
-    # st.sidebar.slider("Response Length", min_value=1, max_value=2048, value=150, step=1)
-
